cad5a7ca786a27bd1186423994151949ac6c7da9
danilovict2 <danilosindric@gmail.com>
2023-01-05 20:18:04 +0100

    working on frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="/images/Instagram_logo.png" class="rounded-circle" style="height: 170px">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Instagram</h1></div>
+            <div class="d-flex">
+                <div style="padding-right: 25px"><strong>153</strong> posts</div>
+                <div style="padding-right: 25px"><strong>23k</strong> followers</div>
+                <div style="padding-right: 25px"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4" style="font-weight: bold">freecodecamp.org</div>
+            <div>We're a global community of millions of people learning to code together.LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+        <div class="row">
+            <div class="col-4">
+                <img src="https://instagram.fbeg4-1.fna.fbcdn.net/v/t51.2885-15/323818807_612146687581193_3474640052081855596_n.jpg?stp=dst-jpg_e35&amp;_nc_ht=instagram.fbeg4-1.fna.fbcdn.net&amp;_nc_cat=105&amp;_nc_ohc=6xxciHTJ28AAX_ySfJI&amp;tn=GvTKG4edVGA05hAw&amp;edm=ACWDqb8BAAAA&amp;ccb=7-5&amp;ig_cache_key=MzAwODM5MTQ2NjE1Nzc5OTg1Ng%3D%3D.2-ccb7-5&amp;oh=00_AfDEOrhWBkvR_NGqRKi39rF7Y_V0Az8X-lB5K3vUQwVCNw&amp;oe=63BC1F2D&amp;_nc_sid=1527a3" alt="">
             </div>
         </div>
+
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <div><img src="/images/Instagram_logo.png" alt="logo"></div>
-                    <div>Instagram</div>
-                    
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <div><img src="/images/Instagram_logo.png" style="height: 18px; padding-right: 1ch; border-right: 1px solid"></div>
+                    <div style="padding-left: 1ch; padding-top: 2.5px">Instagram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
