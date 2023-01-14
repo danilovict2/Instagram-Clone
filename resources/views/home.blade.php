0641205c4a57dc939d3c23162e22ab926586ef3e
danilovict2 <danilosindric@gmail.com>
2023-01-14 22:53:14 +0100

    upd: profile frontend done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,9 +17,15 @@
             <div>We're a global community of millions of people learning to code together.LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
             <div><a href="#">www.freecodecamp.org</a></div>
         </div>
-        <div class="row">
+        <div class="row pt-5">
             <div class="col-4">
-                <img src="https://instagram.fbeg4-1.fna.fbcdn.net/v/t51.2885-15/323818807_612146687581193_3474640052081855596_n.jpg?stp=dst-jpg_e35&amp;_nc_ht=instagram.fbeg4-1.fna.fbcdn.net&amp;_nc_cat=105&amp;_nc_ohc=6xxciHTJ28AAX_ySfJI&amp;tn=GvTKG4edVGA05hAw&amp;edm=ACWDqb8BAAAA&amp;ccb=7-5&amp;ig_cache_key=MzAwODM5MTQ2NjE1Nzc5OTg1Ng%3D%3D.2-ccb7-5&amp;oh=00_AfDEOrhWBkvR_NGqRKi39rF7Y_V0Az8X-lB5K3vUQwVCNw&amp;oe=63BC1F2D&amp;_nc_sid=1527a3" alt="">
+                <img src="/images/323818807_612146687581193_3474640052081855596_n.jpg" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="/images/323818807_612146687581193_3474640052081855596_n.jpg" class="w-100">
+            </div>
+            <div class="col-4">
+                <img src="/images/323818807_612146687581193_3474640052081855596_n.jpg" class="w-100">
             </div>
         </div>
 
